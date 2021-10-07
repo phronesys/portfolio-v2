8278b5f4484a4f244ae8a1284276f527f312b866
phronesys <daniel.dazarola@gmail.com>
2021-10-07 02:57:20 -0300

    enhancement(navigation-bar): scroll to change bg color

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,10 +1,11 @@
 <template>
-  <section>
+  <section :class="{ dark: scrollPosition > 700 }">
     <div>Dev 4 u</div>
     <ul>
       <li>About</li>
       <li>Blog</li>
       <li>Guides</li>
+      <li>{{scrollPosition}}</li>
     </ul>
     <button>Let's create!</button>
   </section>
@@ -13,13 +14,33 @@
 <script>
 export default {
   name: "NavigationBar",
+  props: {
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: () => ({
+    scrollPosition: 0,
+  }),
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
 };
 </script>
 
 <style lang="postcss" scoped>
 section {
-  @apply p-10 w-full h-20 flex flex-row items-center fixed top-0 left-0 right-0 z-10;
-  @apply bg-gradient-to-r from-green-400 to-purple-500 text-white;
+  @apply p-10 w-full h-20 flex flex-row items-center fixed top-0 left-0 right-0 z-20;
+  @apply bg-transparent text-white transition-all duration-200 ease-linear;
+}
+section.dark {
+  @apply bg-black;
 }
 section > div {
   @apply font-semibold text-4xl;
@@ -30,5 +51,4 @@ section > ul {
 section > button {
   @apply bg-green-400 px-6 py-3 rounded-2xl text-white text-xl font-semibold;
 }
-
 </style>
