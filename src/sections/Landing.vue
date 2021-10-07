a0c2e2f311e8a10b0e84691f4c384169abca33f0
phronesys <daniel.dazarola@gmail.com>
2021-10-07 03:02:34 -0300

    enhancement: landing deatils

--- src/sections/Landing.vue
+++ src/sections/Landing.vue
@@ -1,12 +1,15 @@
 <template>
   <section>
     <div>
-      <h1>¿Necesitas una mano con tu negocio online?</h1>
-      <h4>Te ayudaré a construir la página que imaginas</h4>
-      <button>Conversemos</button>
+      <h1>¿Imaginas esa página que destacará tu marca 💪🏪?</h1>
+      <h4>
+        Si aun no lo haces, permíteme ayudar con la imagen y construcción de tu
+        sitio 😉
+      </h4>
+      <button>Conversemos 🚀</button>
     </div>
     <div class="image-1"></div>
-    <div class="image-2"></div>
+    <div class="image-2"><div></div></div>
     <section-divider class="!w-full"></section-divider>
   </section>
 </template>
@@ -24,35 +27,38 @@ export default {
 <style lang="postcss" scoped>
 section {
   @apply bg-gradient-to-r from-green-400 to-purple-500;
-  @apply h-[800px] mt-20 relative flex justify-center items-center pb-40;
+  @apply h-[800px] pt-24 relative flex justify-center items-center pb-80;
 }
 section > div {
-  @apply text-white flex flex-col gap-8 w-[700px] items-center text-center;
+  @apply text-white flex flex-col gap-8 w-[800px] items-center text-center;
 }
 section > div h1 {
   @apply text-6xl font-bold;
 }
 section > div h4 {
-  @apply text-2xl;
+  @apply text-2xl w-2/3;
 }
 section > div button {
   @apply bg-green-400 font-semibold text-xl px-6 py-3 text-white rounded-2xl w-56;
 }
-section >  :is(.image-1, .image-2) {
+section > :is(.image-1, .image-2) {
   @apply absolute bg-gray-50 rounded-3xl h-[300px] w-[300px];
 }
 section > .image-1 {
-  @apply left-20 bottom-5 z-30;
-  background: url('../assets/building_websites.svg');
+  @apply left-44 bottom-5 z-10;
+  background: url("../assets/building_websites.svg");
   background-position: center;
   background-size: contain;
   background-repeat: no-repeat;
 }
 section > .image-2 {
-  @apply right-20 bottom-32;
-  background: url('../assets/mobile_development.svg');
+  @apply right-20 top-32;
+  background: url("../assets/mobile_development.svg");
   background-position: center;
   background-size: contain;
   background-repeat: no-repeat;
+}
+section > .image-2 > div {
+  @apply bg-white w-80 h-80 absolute bottom-[-267px] -right-1 rounded-lg shadow-xl;
 }
 </style>
