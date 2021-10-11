290a96bc7ba6dcf2d8664f1a716c835ec4884a8d
phronesys <daniel.dazarola@gmail.com>
2021-10-11 03:13:12 -0300

    enhancement(projects): background color gradient

--- src/sections/Projects.vue
+++ src/sections/Projects.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="h-[1000px] bg-green-400"></div>
+  <div class="h-[1000px] bg-gradient-to-b from-indigo-100 to-green-400">
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
