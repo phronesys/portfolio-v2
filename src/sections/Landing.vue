c0f8b0a086835e7e919d2e3c2dda7e6be903aca8
phronesys <daniel.dazarola@gmail.com>
2021-11-29 01:16:08 -0300

    change: removed circle

--- src/sections/Landing.vue
+++ src/sections/Landing.vue
@@ -10,11 +10,6 @@
         powerful <span>web applications</span>
       </h3>
     </div>
-    <div class="right">
-      <div class="circle crimson" />
-      <div class="circle purple" />
-      <svg-landing></svg-landing>
-    </div>
   </section>
 </template>
 
@@ -30,8 +25,7 @@ export default {
 
 <style lang="postcss" scoped>
 section {
-  @apply px-8 pt-20 bg-[#020202] h-[700px] w-full text-white overflow-hidden;
-  @apply lg:h-[800px] lg:px-36 lg:flex-row;
+  @apply px-8 pt-20 bg-[#020202] h-[600px] w-full text-white overflow-hidden;
   @apply flex flex-col gap-10;
   & .left {
     @apply flex flex-col justify-center gap-4 z-[2];
@@ -49,29 +43,6 @@ section {
       & span {
         @apply text-purple-600;
       }
-    }
-  }
-  & .right {
-    @apply w-full h-full relative flex flex-row bg-white;
-    & .circle {
-      @apply rounded-full absolute z-[0];
-      &.purple {
-        @apply bg-purple-600;
-        @apply  xl:w-[500px] xl:h-[500px] xl:top-[100px] xl:left-0;
-        @apply  sm:w-[375px] sm:h-[375px] sm:top-[-30px] sm:left-[10px];
-        @apply  w-[275px] h-[275px] top-[30px] left-[10px];
-      }
-      &.crimson {
-        @apply bg-pink-600;
-        @apply xl:w-[475px] xl:h-[475px] xl:bottom-[150px];
-        @apply sm:w-[275px] sm:h-[275px] sm:bottom-[-50px];
-        @apply xl:w-[475px] xl:h-[475px] xl:bottom-[150px];
-        @apply  w-[275px] h-[275px] top-[50px] right-[10px];
-      }
-    }
-    & svg {
-      @apply z-[1] w-[400px] absolute;
-      @apply  w-[275px] h-[275px] top-[30px] left-[10px];
     }
   }
 }
