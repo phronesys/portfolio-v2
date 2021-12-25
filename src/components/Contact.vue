69d8a8577cf681f8983860169c59acd45ec83d23
phronesys <daniel.dazarola@gmail.com>
2021-12-25 18:16:44 -0300

    feat: contact icons

---
+++ src/components/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="contact"></component>
+</template>
+
+<script setup>
+import * as Contact from "./contact/index";
+import { computed } from "vue";
+
+const props = defineProps({
+  name: String,
+});
+
+const contactList = {
+  github: Contact.Github,
+  linkedin: Contact.Linkedin,
+  devto: Contact.Devto,
+  upwork: Contact.Upwork,
+};
+
+const contact = computed(() => contactList[props.name]);
+</script>
