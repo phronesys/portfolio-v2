c3672c053806e039f480e40e61a7254bad2b55bd
phronesys <daniel.dazarola@gmail.com>
2021-12-25 18:17:41 -0300

    refactor: skills renaming

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,32 +1,32 @@
 <template>
-  <component :is="skill"></component> 
+  <component :is="skill"></component>
 </template>
 
 <script setup>
 import * as Skills from "./skills/index";
-import { ref, defineProps, computed } from "vue";
+import { computed } from "vue";
 
 const props = defineProps({
   name: String,
 });
 
 const skills = {
-  chrome: Skills.SkillChrome, 
-  figma: Skills.SkillFigma, 
-  git: Skills.SkillGit, 
-  jest: Skills.SkillJest, 
-  linux: Skills.SkillLinux, 
-  netlify: Skills.SkillNetlify, 
-  node: Skills.SkillNode, 
-  npm: Skills.SkillNpm, 
-  nuxt: Skills.SkillNuxt, 
-  rollup: Skills.SkillRollup, 
-  storybook: Skills.SkillStorybook, 
-  tailwind: Skills.SkillTailwind, 
-  vite: Skills.SkillVite, 
-  vue: Skills.SkillVue, 
-  zsh: Skills.SkillZsh, 
-}
+  chrome: Skills.Chrome,
+  figma: Skills.Figma,
+  git: Skills.Git,
+  jest: Skills.Jest,
+  linux: Skills.Linux,
+  react: Skills.React,
+  node: Skills.Node,
+  npm: Skills.Npm,
+  nuxt: Skills.Nuxt,
+  rollup: Skills.Rollup,
+  storybook: Skills.Storybook,
+  tailwind: Skills.Tailwind,
+  vite: Skills.Vite,
+  vue: Skills.Vue,
+  zsh: Skills.Zsh,
+};
 
-const skill = computed(() => skills[props.name])
+const skill = computed(() => skills[props.name]);
 </script>
