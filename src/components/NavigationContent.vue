be2d20d3306a1053c3ac7099c5c24df6ef216fa1
phronesys <daniel.dazarola@gmail.com>
2022-01-17 23:02:12 -0300

    enhance: navbar buttons close sidebar and scroll

--- src/components/NavigationContent.vue
+++ src/components/NavigationContent.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="nav-content">
     <div class="language">
-      <svg-flag-spain :class="{ selected: !english }"></svg-flag-spain>
-      <base-switch v-model="english"></base-switch>
-      <svg-flag-usa :class="{ selected: english }"></svg-flag-usa>
+      <svg-flag-spain :class="{ selected: !english }" />
+      <base-switch v-model="english" />
+      <svg-flag-usa :class="{ selected: english }" />
     </div>
     <div class="buttons">
-      <base-button small>{{ t("button.about") }}</base-button>
-      <base-button primary small>{{ t("button.contact") }}</base-button>
+      <base-button small @click="clickAbout">
+        {{ t("button.about") }}
+      </base-button>
+      <a href="#contact">
+        <base-button primary small @click="clickContact">
+          {{ t("button.contact") }}
+        </base-button>
+      </a>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, defineEmits } from "vue";
 import { useI18n } from "vue-i18n";
 
 import BaseButton from "./BaseButton.vue";
@@ -21,12 +27,25 @@ import BaseSwitch from "./BaseSwitch.vue";
 import SvgFlagSpain from "./svg/SvgFlagSpain.vue";
 import SvgFlagUsa from "./svg/SvgFlagUsa.vue";
 
+const emit = defineEmits(["close-modal"]);
+
 const english = ref(false);
 
 const { t, locale } = useI18n({
   inheritLocale: true,
 });
-locale.value = "es"
+locale.value = "es";
+
+const closeModal = () => emit("close-modal");
+
+/* button handle */
+const clickAbout = () => {
+  /* open about component */
+  closeModal();
+};
+const clickContact = () => {
+  closeModal();
+};
 </script>
 
 <i18n lang="yaml">
